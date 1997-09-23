a68028a2d4a05cf0af2df030d9767f14cf447872
marki <marki>
1997-09-23 01:28:00 +0000

    start vax calendar program.

---
+++ calendar.tcl
@@ -0,0 +1,25 @@
+#!/usr/bin/expect --
+#
+# get the password
+#
+stty -echo
+send_user "password? "
+expect_user -re "(.*)\n"
+send_user "\n"
+set password $expect_out(1,string)
+#
+# login
+#
+spawn telnet cebaf2
+expect "Username:"
+send "marki\r"
+expect "Password:"
+send "$password\r"
+#
+# start the calendar
+#
+expect "CEBAF2"
+send "dir\r"
+expect "CEBAF2"
+send "calendar\r"
+interact
