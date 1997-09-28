213c348511c6f934376f0fb357b19109760dd1d5
marki <marki>
1997-09-28 23:51:35 +0000

    Lengthen timeout for password to 60 seconds. Add argument, target display. Needed only if not claspc2.

--- calendar.tcl
+++ calendar.tcl
@@ -4,7 +4,9 @@
 #
 stty -echo
 send_user "password? "
+set timeout 60
 expect_user -re "(.*)\n"
+set timeout 10
 send_user "\n"
 set password $expect_out(1,string)
 #
@@ -21,5 +23,9 @@ send "$password\r"
 expect "CEBAF2"
 send "dir\r"
 expect "CEBAF2"
-send "calendar\r"
+if {$argv == ""} {
+    send "calendar\r"
+} else {
+    send "rwx calendar $argv\r"
+}
 interact
