66447bb4dbb0fca5bfbb534be336e6a85daa3690
marki <marki>
1999-07-12 20:23:32 +0000

    working version, puts file list on stdout, no other output

--- work_delete_list.pl
+++ work_delete_list.pl
@@ -1,16 +1,28 @@
 #!/usr/local/bin/perl
 $access_time_min = 4.0;
-#$workdisk[0] = "/w/work501/staylor/.";
-#$workdisk[1] = "/w/work601/fklein/.";
+$free_fraction = 0.2;
 $workdisk[0] = "/w/work501/.";
 $workdisk[1] = "/w/work601/.";
 $iwd = 0;
 while ($workdisk[$iwd]) {
+    $disk = $workdisk[$iwd];
     undef %file_array;
     undef %size_array;
     $size_sum = 0;
-    $target = 10000000000;
-    open(FIND, "find $workdisk[$iwd] -type f |");
+#
+# calculate target to delete
+#
+    open(DF, "df -k $disk |");
+    $line = <DF>;
+    $line = <DF>;
+    @field = split(/\s+/, $line);
+    $size_tot = $field[1];
+    $size_avail = $field[3];
+    $size_delete_target = 1000*($free_fraction*$size_tot - $size_avail);
+#
+# survey the disk
+#
+    open(FIND, "find $disk -type f |");
     while ($file = <FIND>) {
 	chop $file;
 	$access_time = -A $file;
@@ -25,16 +37,19 @@ while ($workdisk[$iwd]) {
 	    $file_array{$access_time} .= $file . '&';
 	}
     }
-    print "arrays\n";
+#
+# loop over the files, oldest first, listing them until target reached
+#
     fileloop: foreach $access_time (reverse sort numerically keys(%file_array)) {
-	print "access_time = $access_time\n";
-	print "file = $file_array{$access_time}\n";
+	#print "access_time = $access_time\n";
+	#print "file = $file_array{$access_time}\n";
 	@files = split(/&/, $file_array{$access_time});
 	foreach $file (@files) {
 	    $size = $size_array{$file};
 	    $size_sum += $size;
-	    print "$file $size $size_sum $target\n";
-	    last fileloop if $size_sum > $target;
+	    #print "$file $size $size_sum $size_delete_target\n";
+	    print "$file\n";
+	    last fileloop if $size_sum > $size_delete_target;
 	}
     }
     $iwd++;
