c03bce758fe4d5bc2f6f58f537b0fdc1084c47d7
marki <marki>
1999-09-08 12:35:34 +0000

    look at mins and maxes of req no's

--- tape_reqs.pl
+++ tape_reqs.pl
@@ -1,8 +1,15 @@
 #!/usr/local/bin/perl
-open(TS, "/site/bin/jtsstat | wc |");
-$info = <TS>;
-chop $info;
-@field = split(/\s+/, $info);
-$nreqs = $field[1];
-print time(), " $nreqs\n";
+open(TS, "/site/bin/jtsstat |");
+$nreqs = 0;
+$reqno_min = 9999999;
+$reqno_max = 0;
+while ($info = <TS>) {
+    chop $info;
+    @field = split(/\s+/, $info);
+    $reqno = $field[0];
+    $nreqs++;
+    if ($reqno < $reqno_min) {$reqno_min = $reqno;}
+    if ($reqno > $reqno_max) {$reqno_max = $reqno;}
+}
+print time(), " $nreqs $reqno_min $reqno_max\n";
 exit 0;
