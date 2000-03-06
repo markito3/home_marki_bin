d3656dc7a775ca90400ed9327a46445288120171
marki <marki>
2000-03-06 16:15:48 +0000

    Rename cache_time.perl to cache_time.pl

--- cache_time.pl
+++ cache_time.pl
@@ -1,6 +1,9 @@
 #!/usr/local/bin/perl
-$cachedisk[0] = "/w/cache501";
-$cachedisk[1] = "/w/cache601";
+$cachedisk[0] = "/w/cache301";
+$cachedisk[1] = "/w/cache402";
+$cachedisk[2] = "/w/cache501";
+$cachedisk[3] = "/w/cache601";
+$access_time_delete = 36;
 $icd = 0;
 $access_time_max = 0;
 while ($cachedisk[$icd]) {
@@ -8,7 +11,8 @@ while ($cachedisk[$icd]) {
     while ($file = <FIND>) {
 	chop $file;
 	$access_time = -A $file;
-	if ($access_time > $access_time_max) {
+	if ($access_time < $access_time_delete
+	    && $access_time > $access_time_max) {
 	    $access_time_max = $access_time;
 	}
         #print "$file $access_time $access_time_max\n";
