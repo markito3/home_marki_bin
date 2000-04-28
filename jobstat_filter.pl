a39b1d77e608233de243c8d9df6c442ce3145a8d
marki <marki>
2000-04-28 13:59:24 +0000

    puts together continuation lines

---
+++ jobstat_filter.pl
@@ -0,0 +1,20 @@
+#!/usr/local/bin/perl
+while (<>) {
+    #print;
+    if (/^Job /) {
+	print $line;
+	$line = $_;
+    } elsif (/^Mon / || /^Tue / || /^Wed / || /^Thu / || /^Fri / || /^Sat / || /^Sun /) {
+	print $line;
+	$line = $_;
+    } elsif (/^                     /) {
+	if (/^                     </) {s/</ </}
+	s/^                     //;
+	$line_continue = $_;
+	chop $line;
+	$line .= $line_continue;
+    }
+}
+print $line;
+exit 0;
+# end of perl script
