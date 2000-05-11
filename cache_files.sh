5d60b5b12945af6e7251ceb6b8cb7b4848f28844
marki <marki>
2000-05-11 16:06:31 +0000

    Check for others

--- cache_files.sh
+++ cache_files.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 ls -lurt `find /w/cache101/ -type f` `find /w/cache201/ -type f` \
-    | perl -e 'while (<>) {s/-rw-r--r--   1 root     bin     //;print;}' \
+    | perl -e 'while (<>) { \
+	s/-rw-r--r--   1 root     bin     //; \
+	s/-rw-r--r--   1 root     others  //; \
+	print;}' \
     | nl
