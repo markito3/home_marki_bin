c3df7bd71783f0a0d48b2456097e87ffb8f0e647
marki <marki>
2000-05-11 16:07:26 +0000

    Check for other not others

--- cache_files.sh
+++ cache_files.sh
@@ -2,6 +2,6 @@
 ls -lurt `find /w/cache101/ -type f` `find /w/cache201/ -type f` \
     | perl -e 'while (<>) { \
 	s/-rw-r--r--   1 root     bin     //; \
-	s/-rw-r--r--   1 root     others  //; \
+	s/-rw-r--r--   1 root     other   //; \
 	print;}' \
     | nl
