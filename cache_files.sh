80b684e8c37cab327d1d01bae7da9702f1fce781
marki <marki>
2000-05-11 17:11:23 +0000

    More white space

--- cache_files.sh
+++ cache_files.sh
@@ -3,5 +3,4 @@ ls -lurt `find /w/cache101/ -type f` `find /w/cache201/ -type f` \
     | perl -e 'while (<>) { \
 	s/-rw-r--r--\s+1 root\s+bin\s+/ /; \
 	s/-rw-r--r--\s+1 root\s+other\s+/ /; \
-	print;}' \
-    | nl
+	print;}'
