511980f13533dbf3b04f37feaf8055e65ed2df06
marki <marki>
2000-05-11 17:33:01 +0000

    allow group write

--- cache_files.sh
+++ cache_files.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 ls -lurt `find /w/cache101/ -type f` `find /w/cache201/ -type f` \
     | perl -e 'while (<>) { \
-	s/-rw-r--r--\s+1 root\s+bin\s+/ /; \
-	s/-rw-r--r--\s+1 root\s+other\s+/ /; \
-	print;}'
+	s/-rw-r.-r--\s+1\s+root\s+/ /; \
+	s/bin|other/ /; \
+	s/\s+/ /; \
+	print;}' \
+    | nl
