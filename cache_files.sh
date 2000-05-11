86e8667ae6bc771f78b0b4f3629e397b323b733b
marki <marki>
2000-05-11 17:41:17 +0000

    Moved to tools/misc

--- cache_files.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-ls -lurt `find /w/cache101/ -type f` `find /w/cache201/ -type f` \
-    | perl -e 'while (<>) { \
-	s/-rw-r.-r--\s+1\s+root\s+/ /; \
-	s/bin|other/ /; \
-	s/\s+/ /; \
-	print;}' \
-    | nl
