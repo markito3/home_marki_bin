c055c35cf49e05a2dffc86ac50b4e53926854039
marki <marki>
2000-11-21 14:11:18 +0000

    fix the sort

--- js.sh
+++ js.sh
@@ -4,7 +4,7 @@ report_file=$webdir/jobstat.txt
 temp_file=/tmp/jobstat.txt
 rm -f $temp_file # just in case
 date > $temp_file
-/site/bin/jobstat -l -u all | /home/marki/bin/jobstat_cat.pl | /home/marki/bin/jobstat_filter.pl | sort -k6r -k1n >> $temp_file
+/site/bin/jobstat -l -u all | /home/marki/bin/jobstat_cat.pl | /home/marki/bin/jobstat_filter.pl | sort -k 6,6r -k 1,1n >> $temp_file
 rm -f $report_file
 mv $temp_file $report_file
 # end of shell script
