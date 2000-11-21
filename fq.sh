e114041038332967302b3ee038db0a6de847cb90
marki <marki>
2000-11-21 18:11:49 +0000

    add low_priority

--- fq.sh
+++ fq.sh
@@ -4,7 +4,7 @@ report_file=$webdir/farmqueues.txt
 rm -f $report_file
 date > $report_file
 /site/bin/farmqueues >> $report_file
-for queue in priority production jcache idle FARM_TEST
+for queue in priority production jcache low_priority idle FARM_TEST
     do /site/bin/farmqueues -l -r $queue \
 	| grep -v ' 0        0         0.0        0' >> $report_file
 done
