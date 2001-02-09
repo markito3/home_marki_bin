bf18ee00a947a63e6f07681a5d68ab816a7251bf
marki <marki>
2001-02-09 12:38:47 +0000

    Working version

--- grepmail.pl
+++ grepmail.pl
@@ -7,15 +7,32 @@ foreach $file (@ARGV) {
     open(INPUT, $file); # open the file
     $nline = 0;
     $nmesg = 0;
+    $match = 0;
+    @message = ""; # clear array?
     while ($line = <INPUT>) {
 	$nline++;
 	if ($line =~ /^From /) {
 	    $nmesg++;
+	    if ($match) {PrintMessage();}
+	    @message = "";
+	    $match = 0;
 	    print $line;
+	} else {
+	    push(@message, $line);
+	    if ($line =~ /$pattern/) {$match = 1}
 	}
     }
+    if ($match) {PrintMessage();}
     print "nline=$nline\n";
     print "nmesg=$nmesg\n";
     close(INPUT);
 }
 exit 0;
+
+sub PrintMessage {
+    if (! @message) {return;}
+    print "----------start of message-------------\n";
+    print @message;
+    print "----------end of message---------------\n";
+    return;
+}
