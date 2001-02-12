a3538ce1eeb98b9e11d2dfa04382b0411d191050
marki <marki>
2001-02-12 02:50:31 +0000

    Print out key info lines from the mail preamble, and nothing else from the preamble.

--- grepmail.pl
+++ grepmail.pl
@@ -4,22 +4,33 @@ $pattern = shift(@ARGV); # pattern to search for
 print "/pattern/=/$pattern/\n";
 foreach $file (@ARGV) {
     print "file=$file\n";
+    print "<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n";
     open(INPUT, $file); # open the file
     $nline = 0;
     $nmesg = 0;
     $match = 0;
-    @message = ""; # clear array?
+    $preamble = 0;
+    @info = "";
+    @message = "";
     while ($line = <INPUT>) {
 	$nline++;
 	if ($line =~ /^From /) {
 	    $nmesg++;
 	    if ($match) {PrintMessage();}
-	    @message = "";
 	    $match = 0;
-	    print $line;
+	    $preamble = 1;
+	    @message = "";
+	    @info = "";
+	} elsif ($preamble) {
+	    if ($line =~ /^Date: / || $line =~ /^From: / || $line =~ /^To: /
+		|| $line =~ /^Subject: /) {
+		push(@info, $line);
+	    } elsif ($line eq "\n") {
+		$preamble = 0;
+	    }
 	} else {
 	    push(@message, $line);
-	    if ($line =~ /$pattern/) {$match = 1}
+	    if ($line =~ /$pattern/i) {$match = 1}
 	}
     }
     if ($match) {PrintMessage();}
@@ -31,8 +42,10 @@ exit 0;
 
 sub PrintMessage {
     if (! @message) {return;}
-    print "----------start of message-------------\n";
+    print @info;
+    print "--------------------------start of message----------------------\n";
     print @message;
-    print "----------end of message---------------\n";
+    print "---------------------------end of message-----------------------\n";
+    print "<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n";
     return;
 }
