2c238e1dbd5364584b653926ece323a2a47f4b5b
marki <marki>
2001-07-06 19:50:21 +0000

    Make a temp. file first; rename to real file on completion.

--- cf.sh
+++ cf.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 webdir=/home/marki/public_html/clas
 report_file=$webdir/cache_files.txt
-rm -f $report_file
-date > $report_file
-/group/clas/tools/misc/cache_files.pl >> $report_file
+temp_file=$webdir/cache_files.tmp
+rm -f $temp_file
+date > $temp_file
+/group/clas/tools/misc/cache_files.pl >> $temp_file
+mv $temp_file $report_file
 # end of shell script
