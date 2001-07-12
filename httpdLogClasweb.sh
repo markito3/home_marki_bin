dd4abbe1d3ca31873c5059ff7ffe9093f9a8d150
marki <marki>
2001-07-12 19:41:47 +0000

    screen more stuff

--- httpdLogClasweb.sh
+++ httpdLogClasweb.sh
@@ -2,5 +2,6 @@
 ssh -n clasweb tail -1000 /var/log/httpd/access.log \
     | grep -v clasun | grep -v ccl3 | grep -v claspc2 | grep -v googlebot \
     | grep -v /elton | grep -v .gif | grep -v clastest.servlet \
-    | grep -v marvin.northernlight.com
+    | grep -v marvin.northernlight.com | grep -v inktomi \
+    | grep -v excite.com | grep -v watcher.jlab.org
 # end of Bourne shell script
