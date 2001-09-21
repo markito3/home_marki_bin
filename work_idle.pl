88663426d3b916aec2a9fac83be1c1767046a8d4
marki <marki>
2001-09-21 18:47:38 +0000

    Limit stale size to less than used size.

--- work_idle.pl
+++ work_idle.pl
@@ -15,6 +15,7 @@ if (!$#field) {
 $size = $field[1]/1.e6;
 $used = $field[2]/1.e6;
 $free = $field[3]/1.e6;
+if ($stale > $used) {$stale = $used}
 if ($ARGV[1]) {
     printf "%25s  %6s  %6s  %6s  %6s\n",
 	("disk partition", "size", "used", "stale", "free");
