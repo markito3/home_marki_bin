f2fcbceaad3232a05adf6652373c8fae88943b2a
marki <marki>
2001-11-13 15:10:40 +0000

    tighten fraction cut from .70 to .75: fewer warnings

--- work_warn_db.pl
+++ work_warn_db.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 #
-# $Id: work_warn_db.pl,v 1.7 2001/11/09 04:07:46 marki Exp $
+# $Id: work_warn_db.pl,v 1.8 2001/11/13 15:10:40 marki Exp $
 ########################################################################
 
 use DBI;
@@ -9,7 +9,7 @@ use DBI;
 eval "\$$1=\$2" while $ARGV[0] =~ /^(\w+)=(.*)/ && shift; # see camel book
 
 # initialize constant
-$used_fraction_target = 0.70;
+$used_fraction_target = 0.75;
 $access_age_cut = 5.0; # days
 $atime_big = 365*100;
 
