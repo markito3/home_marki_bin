5bf79e3e11e8e2a591c3eba60554fa0e0989f2b5
marki <marki>
2001-11-21 16:17:54 +0000

    add units to hours column explanation

--- js.sh
+++ js.sh
@@ -5,7 +5,7 @@ temp_file=/tmp/jobstat.txt
 rm -f $temp_file # just in case
 date > $temp_file
 echo >> $temp_file
-echo Columns are: job_no jobname user project status queue submit_time start_time node cpu_time. >> $temp_file
+echo Columns are: job_no jobname user project status queue submit_time start_time node cpu_time\(hours\). >> $temp_file
 echo If job pending, dependency condition follows submit_time. >> $temp_file
 echo >> $temp_file
 /site/bin/jobstat -l -u all | /home/marki/bin/jobstat_cat.pl | /home/marki/bin/jobstat_filter.pl | sort -k 6,6r -k 1,1n >> $temp_file
