7e8cae2079c778112451e581072b980fe8a344f3
marki <marki>
2002-06-10 15:32:37 +0000

    Only report requests queued within the last week.

--- ss.sh
+++ ss.sh
@@ -3,5 +3,5 @@ webdir=/home/marki/public_html/clas
 report_file=$webdir/silo_status.txt
 rm -f $report_file
 date > $report_file
-/group/clas/tools/misc/silo_status.pl >> $report_file
+/group/clas/tools/misc/silo_status.pl time_cut=7 >> $report_file
 # end of shell script
