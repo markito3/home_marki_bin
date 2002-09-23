0952aee86bb7ca4bc896bcff0b839f34e7deb4a7
marki <marki>
2002-09-23 18:32:54 +0000

    Print out the versions being diff'ed

--- crondiff.sh
+++ crondiff.sh
@@ -2,5 +2,6 @@
 rm -f /tmp/crondiff.tmp
 crontab -l | grep -v 'DO NOT EDIT' | grep -v 'installed on' | \
     grep -v 'Cron version' > /tmp/crondiff.tmp
+echo diff /home/marki/misc/crontab.txt crontab
 diff /home/marki/misc/crontab.txt /tmp/crondiff.tmp 
 exit
