4dcc29366a41b5a22f2674aa2df9d39c99e72706
marki <marki>
2002-10-03 18:09:35 +0000

    Moved to tools/status in CLAS repository

--- cu.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-webdir=/home/marki/public_html/clas
-report_file=$webdir/cache_use.txt
-temp_file=/tmp/cache_use.txt
-rm -f $temp_file # just in case
-date > $temp_file
-/home/marki/bin/cache_use.pl >> $temp_file
-mv $temp_file $report_file
-# end of shell script
