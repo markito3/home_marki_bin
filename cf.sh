af8a9f2b293fcaa6d66a706975f33ac29ef67497
marki <marki>
2002-10-06 17:09:02 +0000

    Move to public repository.

--- cf.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-webdir=/home/marki/public_html/clas
-report_file=$webdir/cache_files.txt
-temp_file=$webdir/cache_files.tmp
-rm -f $temp_file
-date > $temp_file
-/group/clas/tools/misc/cache_files.pl >> $temp_file
-mv $temp_file $report_file
-# end of shell script
