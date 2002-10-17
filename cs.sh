6c5decb496b4b5d9d04c9ca32391f1260f604acb
marki <marki>
2002-10-17 00:38:02 +0000

    Moving to claslib@ifarml1

--- cs.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-webdir=/home/marki/public_html/clas
-report_file=$webdir/cache_status.txt
-rm -f $report_file
-date > $report_file
-/group/clas/tools/misc/cache_status.pl >> $report_file
-# end of shell script
--- ss.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-webdir=/home/marki/public_html/clas
-report_file=$webdir/silo_status.txt
-rm -f $report_file
-date > $report_file
-/group/clas/tools/misc/silo_status.pl time_cut=7 >> $report_file
-# end of shell script
