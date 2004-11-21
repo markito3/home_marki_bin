35ccdd3cdbf2c7981bd71f3f221f37899842b5c5
marki <marki>
2004-11-21 17:34:11 +0000

    auto log into jlab

---
+++ jlab.tcl
@@ -0,0 +1,13 @@
+#!/usr/bin/expect --
+set ioc $argv
+set passwd "********"
+set timeout 30
+spawn slogin jlab.jlab.org
+expect "password"
+send "$passwd\r"
+expect "bash"
+send "ssh $ioc\r"
+expect "password"
+send "$passwd\r"
+expect ">"
+interact
