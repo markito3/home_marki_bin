f7a284e119605c204e6f158843439b7fd8f923fb
marki <marki>
2006-07-27 21:52:49 +0000

    explicit path to vncreflector

--- vncreflector.sh
+++ vncreflector.sh
@@ -2,4 +2,4 @@
 #
 # starts vncreflector
 #
-vncreflector -l 5901 -p ~/.vnc_reflector/passwd.txt ~/.vnc_reflector/host_info.txt
+/usr/local/vnc_reflector/vncreflector -l 5901 -p ~/.vnc_reflector/passwd.txt ~/.vnc_reflector/host_info.txt
