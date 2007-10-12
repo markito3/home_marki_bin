1cc5fd143efbb590ae1135d1b997fd54753c0bc7
marki <marki>
2007-10-12 19:52:06 +0000

    check for a running logbot

---
+++ logbot_check.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+open (PS, "ps x |");
+$n = 0;
+while (<PS>) {
+    if (/java org\/jibble\/logbot\/LogBotMain/) {
+	$n++;
+    }
+}
+if ($n == 0) {
+print "logbot not running\n";
+}
+exit 0;
