340b5dcf4363644c5a9e46294aa680dc35e9d7e1
marki <marki>
2007-11-09 13:33:23 +0000

    silent version

--- backup.pl
+++ backup.pl
@@ -2,7 +2,7 @@
 $format = $ARGV[0];
 $target_dir = "/u/scratch/marki/home/`date $format`/";
 system "mkdir -p $target_dir";
-$rsync_command = "rsync -ruv --exclude='Music/**' /home/marki $target_dir";
-print "rsync_command = ", $rsync_command, "\n";
+$rsync_command = "rsync -ruq --exclude='Music/**' --exclude='Download/**' --exclude='.*/**' /home/marki $target_dir";
+#print "rsync_command = ", $rsync_command, "\n";
 system $rsync_command;
 exit 0;
