39ec0b6a607e114335b29e26d08c5145ce6c2f0d
marki <marki>
2007-11-15 14:15:51 +0000

    make a log of the backup in the backup directory

--- backup.pl
+++ backup.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl -w
 $format = $ARGV[0];
-$target_dir = "/u/scratch/marki/home/`date $format`/";
+$target_dir = "/u/scratch/marki/home/`date $format`";
 system "mkdir -p $target_dir";
-$rsync_command = "rsync -ruq --exclude='Music/**' --exclude='Download/**' --exclude='.*/**' /home/marki $target_dir";
+$rsync_command = "rsync -ruv --exclude='Music/**' --exclude='Download/**' --exclude='.*/**' /home/marki $target_dir/";
 #print "rsync_command = ", $rsync_command, "\n";
-system $rsync_command;
+system "mv $target_dir/backup.log $target_dir/backup.log.previous";
+system "$rsync_command > $target_dir/backup.log";
 exit 0;
