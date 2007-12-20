fb6219e807a1832bb4d837a6f74e716a8a5e942c
marki <marki>
2007-12-20 21:48:28 +0000

    backup the . directories

--- backup.pl
+++ backup.pl
@@ -6,7 +6,7 @@ if ($ARGV[0]) {
 }
 $target_dir = "/u/scratch/marki/home/`date $format`";
 system "mkdir -p $target_dir";
-$rsync_command = "rsync -ruvt --delete --exclude='Music/**' --exclude='Download/**' --exclude='.*/**' /home/marki $target_dir/";
+$rsync_command = "rsync -ruvt --delete --exclude='Music/**' --exclude='Download/**' --exclude='.Trash/**' --exclude='.beagle/**' --exclude='.purple/logs/**' --exclude='.mozilla/firefox/**' /home/marki $target_dir/";
 #print "rsync_command = ", $rsync_command, "\n";
 system "mv $target_dir/backup.log $target_dir/backup.log.previous";
 system "$rsync_command > $target_dir/backup.log";
