0076669677a9cd3d786f81db675961920551dd4a
marki <marki>
2008-02-09 03:03:59 +0000

    no second pw

--- vnc_tunnel.tcl
+++ vnc_tunnel.tcl
@@ -6,7 +6,7 @@ spawn ssh -t -L5901:localhost:5901 login.jlab.org \
     ssh -t -L5901:localhost:5901 roentgen
 expect "password"
 send "$passwd\r"
-expect "password"
-send "$passwd2\r"
+#expect "password"
+#send "$passwd2\r"
 expect ">"
 interact
