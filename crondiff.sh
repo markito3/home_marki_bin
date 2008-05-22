1194da401fc9b468f1e5d27e4df6166bad1beef3
marki <marki>
2008-05-22 13:02:05 +0000

    use -s switch to diff

--- crondiff.sh
+++ crondiff.sh
@@ -3,5 +3,5 @@ rm -f /tmp/crondiff.tmp
 crontab -l | grep -v 'DO NOT EDIT' | grep -v 'installed on' | \
     grep -v 'Cron version' > /tmp/crondiff.tmp
 echo diff /home/marki/misc/crontab.txt crontab
-diff /home/marki/misc/crontab.txt /tmp/crondiff.tmp 
+diff -s /home/marki/misc/crontab.txt /tmp/crondiff.tmp 
 exit
