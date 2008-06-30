ca741a4cdc0ba3f8953cd2abd5700612bcd10e68
marki <marki>
2008-06-30 20:45:42 +0000

    changes

--- osversion.pl
+++ osversion.pl
@@ -2,29 +2,30 @@
 $uname = `uname`;
 chomp $uname;
 if ($uname eq 'Linux') {
-    if (-f '/etc/fedora-release') {
+    if (-e '/etc/fedora-release') {
 	$release_string = `cat /etc/fedora-release`;
-	chomp $release_string;
-	split(/\s+/, $release_string);
-	$release = $_[0]. $_[2];
-	print "${uname}_$release\n";
-    } elsif (-f '/etc/redhat-release') {
-	$release_string = `cat /etc/fedora-release`;
-	if ($release_string =~ /^Red Hat Enterprise Linux WS release 3.*/) {
-	    $release = 'RHEL3';
-	} elsif ($release_string =~ /^Red Hat Enterprise Linux Client release 5.*/) {
-	    $release = 'RHEL5';
+	if ($release_string =~ /^Fedora release 9.*/) {
+	    $release = '_Fedora9';
 	} else {
-	    $release = 'RH';
+	    $release = '_Fedora';
 	}
-	print "${uname}_$release\n";
+    } elsif (-e '/etc/redhat-release') {
+	$release_string = `cat /etc/redhat-release`;
+	if ($release_string =~ /^Red Hat Enterprise Linux WS release 3.*/) {
+	    $release = '_RHEL3';
+	} elsif ($release_string =~ /^Red Hat Enterprise Linux Client release 5.*/) {
+	    $release = '_RHEL5';
+	} else {
+	    $release = '_RH';
+	}
     } else {
-	print "$uname\n";
+	$release = '';
     }
 } elsif ($uname eq 'SunOS') {
     $release = `uname -r`;
     chomp $release;
-    print "SunOS_$release\n";
 } else {
-    print "$uname\n";
+    $release = '';
 }
+print "${uname}${release}\n";
+exit;
