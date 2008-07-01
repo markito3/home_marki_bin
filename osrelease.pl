a1b1066ece298f1bf4565ba52863d629c4525cd3
marki <marki>
2008-07-01 11:58:07 +0000

    add Ubuntu

--- osrelease.pl
+++ osrelease.pl
@@ -6,10 +6,8 @@ if ($uname eq 'Linux') {
 	$release_string = `cat /etc/fedora-release`;
 	if ($release_string =~ /^Fedora release 9.*/) {
 	    $release = '_Fedora9';
-	} elsif ($release_string =~ /^Fedora release 8.*/) {
+	} elsif ($release_string =~ /Fedora release 8.*/) {
 	    $release = '_Fedora8';
-	} elsif ($release_string =~ /^Fedora release 7.*/) {
-	    $release = '_Fedora7';
 	} else {
 	    print STDERR "unrecognized Fedora release\n";
 	    $release = '_Fedora';
@@ -23,6 +21,24 @@ if ($uname eq 'Linux') {
 	} else {
 	    print STDERR "unrecognized Red Hat release\n";
 	    $release = '_RH';
+	}
+    } elsif (-f '/etc/lsb-release') {
+	$description = `grep DISTRIB_DESCRIPTION /etc/lsb-release`;
+	if ($description) {
+	    if ($description =~ /Ubuntu 8/) {
+		$release = '_Ubuntu8';
+	    } else {
+		$id = `grep DISTRIB_ID /etc/lsb-release`;
+		chomp $id;
+		if ($id) {
+		    split(/=/, $id);
+		    $release = '_' . $_[1];
+		} else {
+		    $release = '';
+		}
+	    }
+	} else {
+	    $release = '';
 	}
     } else {
 	$release = '';
