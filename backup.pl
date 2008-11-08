ff919b23810b7c00c4f75ea1469809d8bdd93114
marki <marki>
2008-11-08 20:33:51 +0000

    changes

--- backup.pl
+++ backup.pl
@@ -51,6 +51,7 @@ $rsync_command .= " --exclude='.dbus'";
 $rsync_command .= " --exclude='.evolution'";
 $rsync_command .= " --exclude='.wapi'";
 $rsync_command .= " --exclude='.tomboy/addin*'";
+$rsync_command .= " --exclude='Documents_work/**'";
 $rsync_command .= " $source $target_dir/";
 if (-e $logfile) {
     #print "$logfile exists\n";
