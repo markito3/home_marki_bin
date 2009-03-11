d0561770261545a0761397509fa11975a08efb82
marki <marki>
2009-03-11 13:00:13 +0000

    scripts to switch screen resolutions between desktop and laptop

---
+++ big.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+xrandr --output HDMI-1 --auto
+xrandr --output LVDS --off
+xrandr --size 1680x1050
+exit 0;
---
+++ small.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+xrandr --output LVDS --auto
+xrandr --output HDMI-1 --off
+xrandr --size 1280x800
+exit 0;
