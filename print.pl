2fb8b06f3c1659fe4e34e02b69bf61ceba60e770
marki <marki>
2010-02-23 13:55:04 +0000

    print through a ssh tunnel

---
+++ print.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+$file_with_path = $ARGV[0];
+@field = split(/\//, $file_with_path);
+$file = $field[$#field];
+$status = system "scp -P9001 $file_with_path localhost:/scratch/marki/lpdir/";
+print "copy status = $status\n";
+if ($status == 0) {
+    $status = system "ssh -p9001 localhost lp -dopmtek1 /scratch/marki/lpdir/$file";
+    print "lp status = $status\n";
+}
+exit;
