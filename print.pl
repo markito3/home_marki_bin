cff6ad574a748974a0254ad1f32f6f50e0b76e09
marki <marki>
2010-03-24 20:38:17 +0000

    double-sided printing

--- print.pl
+++ print.pl
@@ -5,7 +5,7 @@ $file = $field[$#field];
 $status = system "scp -P9001 $file_with_path localhost:/scratch/marki/lpdir/";
 print "copy status = $status\n";
 if ($status == 0) {
-    $status = system "ssh -p9001 localhost lp -dopmtek1 /scratch/marki/lpdir/$file";
+    $status = system "ssh -p9001 localhost lp -dopmtek1 -o sides=two-sided-long-edge /scratch/marki/lpdir/$file";
     print "lp status = $status\n";
 }
 exit;
