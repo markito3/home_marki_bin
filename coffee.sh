b29e542bd63485e0ae8e8bb91e8a6c6d62c4909e
marki <marki>
2010-10-08 17:45:51 +0000

    custom timer

---
+++ coffee.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+seconds=$((60*13))
+sleep $seconds
+display /home/marki/misc/ist2153798coffeecupoe6.jpg &
+xmms /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga > /dev/null
+exit
