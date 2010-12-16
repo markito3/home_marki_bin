9be469037fedd7f17e2160eacd6c6706e0205375
marki <marki>
2010-12-16 14:57:26 +0000

    info message printed

--- gsr.pl
+++ gsr.pl
@@ -3,6 +3,7 @@ $target = $ARGV[0];
 $replacement = $ARGV[1];
 $file = $ARGV[2];
 if (!$file) {print_help(); exit 1;}
+print "gsl.pl: making backup for change: $target -> $replacement\n";
 system("cp -pv $file $file~");
 open(ORIGINAL, "$file~");
 open(EDITTED, ">$file");
