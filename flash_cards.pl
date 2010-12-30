b9b5014e2ebc5e1364cd8f0d7ce56b1bcb2bf310
marki <marki>
2010-12-30 17:41:35 +0000

    no divide by zero, all division facts

--- flash_cards.pl
+++ flash_cards.pl
@@ -37,7 +37,7 @@ sub get_a_card() {
 	if ($operation eq '+'
 	    || $operation eq '-'
 	    || $operation eq '&times;'
-	    || $operation eq '&divide;' && $n1 <= 7 && $n2 <= 7) {
+	    || $operation eq '&divide;' && $n2 != 0) {
 	    $not_a_good_card = 0;
 	}
     }
