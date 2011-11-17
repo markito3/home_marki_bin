5d0f1e25522c23e8e9ac32b879e44a27e85cb421
marki <marki>
2011-11-17 02:33:56 +0000

    almost

--- web2text.sh
+++ web2text.sh
@@ -2,7 +2,7 @@
 f=$1
 url=$2
 rm -f $f
-lynx -dump $url | grep -v edit] | grep -v action=edit | grep -v Visible\ links > $f
+lynx -dump $url | grep -v action=edit | grep -v Visible\ links > $f
 emacs $f
 echo $f
 exit
