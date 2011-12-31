44612622992a79d7a63371ed6f2ad07e1eb5269c
marki <marki>
2011-12-31 19:07:48 +0000

    changes

--- flash_cards_written.pl
+++ flash_cards_written.pl
@@ -8,7 +8,7 @@ print header,
     "<ol>\n";
 
 
-for ($i = 1; $i <= 50; $i++) {
+for ($i = 1; $i <= 12; $i++) {
     $card = get_a_card();
     print "<li> $card\n";
 }
@@ -19,18 +19,27 @@ exit;
 sub get_a_card() {
     $not_a_good_card = 1;
     while ($not_a_good_card) {
-	$n1 = get_a_number();
-	$n2 = get_a_number();
 	$operation = get_an_operation();
-	if ($operation eq '+' || $operation eq '&times;') {
+	if ($operation eq '+') {
+	    $n1 = get_a_number(100);
+	    $n2 = get_a_number(100);
 	    $op1 = $n1;
 	    $op2 = $n2;
 	} elsif ($operation eq '-') {
+	    $n1 = get_a_number(100);
+	    $n2 = get_a_number(100);
 	    $op1 = $n1 + $n2;
 	    $op2 = $n1;
+	} elsif ($operation eq '&times;') {
+	    $n1 = get_a_number(100);
+	    $n2 = get_a_number(100);
+	    $op1 = $n1;
+	    $op2 = $n2;
 	} elsif ($operation eq '&divide;') {
+	    $n1 = get_a_number(100);
+	    $n2 = get_a_number(10);
 	    $op1 = $n1 * $n2;
-	    $op2 = $n1;
+	    $op2 = $n2;
 	} else {
 	    die "bad operation";
 	}
@@ -48,7 +57,7 @@ sub get_a_card() {
 }
 
 sub get_a_number {
-    $max = 10;
+    my ($max) = @_;
     $ranno = rand($max);
     $result = int($ranno);
     return $result;
@@ -56,14 +65,14 @@ sub get_a_number {
 
 sub get_an_operation {
     @operations = ('+', '-', '&times;', '&divide;');
-    $max = 6.0;
+    $max = 4.0;
     $ranno = rand($max);
     #print "ranno = $ranno ";
     if ($ranno < 1.0) {
 	$index = 0;
     } elsif (1.0 <= $ranno && $ranno < 2.0) {
 	$index = 1;
-    } elsif (2.0 <= $ranno && $ranno < 4.0) {
+    } elsif (2.0 <= $ranno && $ranno < 3.0) {
 	$index = 2;
     } else {
 	$index = 3;
