5c540152bffe3f0adc80e508b2665796c9738377
marki <marki>
2012-03-22 13:32:16 +0000

    changes

--- alarm.sh
+++ alarm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 seconds=$((60*$1))
 /home/marki/bin/sleeper.pl $seconds
-display /home/marki/misc/alarmclock.png &
+flash.sh &
 if [ -z $2 ]
     then
     xmms /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga > /dev/null &
