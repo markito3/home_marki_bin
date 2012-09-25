593914b17e3a086baecba059e5e567aff5c3df84
marki <marki>
2012-09-25 15:17:47 +0000

    escape blanks

--- print.pl
+++ print.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/env perl
 $file_with_path = $ARGV[0];
+$file_with_path =~ s/ /\\ /g;
 @field = split(/\//, $file_with_path);
 $file = $field[$#field];
 $status = system "scp -P9003 $file_with_path localhost:/scratch/marki/lpdir/";
 print "copy status = $status\n";
 if ($status == 0) {
-    $status = system "ssh -p9003 localhost lp -dopmtek1 -o sides=two-sided-long-edge /scratch/marki/lpdir/$file";
+    $status = system "ssh -p9003 localhost lp -dopmtek1 -o sides=two-sided-long-edge \'/scratch/marki/lpdir/$file\'";
     print "lp status = $status\n";
 }
 exit;
