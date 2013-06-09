6dbec9079ac3bb41749ecb9f47c856c67d44710a
marki <marki>
2013-06-09 16:19:35 +0000

    perl version, escapes more stuff

---
+++ del_empty_dir.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+open (FIND, "find -type d|");
+while (<FIND>) {
+    chomp;
+    $dir = $_;
+    $dir =~ s/ /\\ /g;
+    $dir =~ s/\(/\\\(/g;
+    $dir =~ s/\)/\\\)/g;
+    $dir =~ s/\'/\\\'/g;
+    $dir =~ s/\&/\\\&/g;
+    $dir =~ s/\"/\\\"/g;
+#    print "====== dir = $dir =========\n";
+    $count = `ls -a $dir | wc -l`;
+    chomp $count;
+    if ($count == 2) {
+	system "rmdir -v $dir\n";
+	}
+}
