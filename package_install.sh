40e6befea6559b54e670ca678d7771bb6c61f423
marki <marki>
2013-12-23 02:20:28 +0000

    packages to add, from f18 and f19

---
+++ package_install.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+sudo yum -y install subversion
+sudo yum -y install thunderbird
+sudo yum -y install cvs
+sudo yum -y install tcsh
+sudo yum -y install perl
+sudo yum -y install gnote
+sudo yum -y install xterm
+sudo yum -y install xorg-x11-apps
+sudo yum -y install alacarte
+sudo yum -y install libgnome
+sudo yum -y install flash-plugin
+sudo yum -y install dconf-editor
+sudo yum -y install xmms
+sudo yum -y install gnome-tweak-tool # allows adjustment of focus-follows-mouse, window titlebar click actions, etc.
+sudo yum -y install gnumeric
+sudo yum -y install wine
+sudo yum -y install abiword
+sudo yum -y install gcc-c++ # C++ compiler
+sudo yum -y install imake # for cernlib build
+sudo yum -y install gcc-gfortran # FORTRAN compiler
+sudo yum -y install libXt-devel # for X11/StringDefs.h for cernlib
+sudo yum -y install motif-devel # for Xm/Xm.h for cernlib
+sudo yum -y install libXpm-devel # for X11/xpm.h for ROOT
+sudo yum -y install expect # for legacy geant4 build
+sudo yum -y install scons # for ccdb build
+sudo yum -y install bzip2-devel # for bzlib.h for sim-recon
+sudo yum -y install lapack-static # for cernlib
+sudo yum -y install blas-static # for cernlib
+sudo yum -y install mysql-devel # for ccdb
+sudo yum -y install revelation
+sudo yum -y install emacs
+sudo yum -y install xorg-x11-fonts-\*
+sudo yum -y install recoll
+sudo yum -y install mailx
+sudo yum -y install detex # helper program for recoll
+sudo yum -y install unrtf # for recoll
+sudo yum -y install perl-Image-ExifTool # for recoll
+sudo yum -y install python-mutagen # for recoll
+sudo yum -y install antiword # for recoll
+sudo yum -y install texlive # for recoll and latex
+sudo yum -y install sound-juicer # for ripping CDs
+sudo yum -y install lynx
+sudo yum -y install mysql-server
+sudo yum -y install perl-XML-Parser
+sudo yum -y install wget
+sudo yum -y install gimp
+sudo yum -y install gnome-media-apps
+sudo yum -y install planner
+sudo yum -y install gimp
+sudo yum -y install perl-XML-Simple
+sudo yum -y install iotop
+sudo yum -y install tigervnc
+sudo yum -y install tigervnc-server
+sudo yum -y install taskcoach
+sudo yum -y install gnucash
+sudo yum -y install grip
+sudo yum -y install latex2html
+sudo yum -y install openssl-perl
+sudo yum -y install httpd
+sudo yum -y install texlive-diagbox
