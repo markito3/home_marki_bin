9b4fc72636b106df315a7c608291754bd0259557
marki <marki>
2014-03-11 16:03:15 +0000

    works

--- diffdir.pl
+++ diffdir.pl
@@ -1,4 +1,14 @@
 #!/usr/bin/env perl
 $dir1 = $ARGV[0];
 $dir2 = $ARGV[1];
-find $dir1 -type f
+open(FIND1, "find $dir1 -maxdepth 1 -type f |");
+while (<FIND1>) {
+    print;
+    chomp;
+    @token = split(/\//);
+    $file = $token[$#token];
+    print "================$file=================\n";
+    system ("diff -s $dir1/$file $dir2/$file");
+}
+exit;
+
