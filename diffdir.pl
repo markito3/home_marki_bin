2f445cd4a57e8859f5c12fb2fe7c28e71bdb48df
marki <marki>
2014-03-26 20:22:52 +0000

    diff files in subdirectories recursively

--- diffdir.pl
+++ diffdir.pl
@@ -1,14 +1,20 @@
 #!/usr/bin/env perl
 $dir1 = $ARGV[0];
 $dir2 = $ARGV[1];
-open(FIND1, "find $dir1 -maxdepth 1 -type f |");
-while (<FIND1>) {
-    print;
-    chomp;
-    @token = split(/\//);
+open(FIND1, "find $dir1 -type f |");
+while ($full = <FIND1>) {
+    chomp $full;
+    @token = split(/\//, $full);
     $file = $token[$#token];
-    print "================$file=================\n";
-    system ("diff -s $dir1/$file $dir2/$file");
+    @token_base0 = split(/$dir1/, $full);
+    $base0 = $token_base0[1];
+    $file_for_split = $file;
+    $file_for_split =~ s/\+/\\\+/g;
+    $file_for_split =~ s/\./\\\./g;
+    @token_base = split(/$file_for_split/, $base0);
+    $base = $token_base[0];
+    print "================$base/$file=================\n";
+    system ("diff -s $dir1/$base/$file $dir2/$base/$file");
 }
 exit;
 
