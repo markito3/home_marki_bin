f42ce13423ea036605de012dc2bc9f1009fd3550
marki <marki>
2014-04-20 23:45:37 +0000

    add offset option

--- random.pl
+++ random.pl
@@ -1,8 +1,13 @@
 #! /usr/bin/env perl
 $max = $ARGV[0];
+if ($ARGV[1]) {
+    $offset = $ARGV[1];
+} else {
+    $offset = 0;
+}
 #print "max = $max\n";
 $ranno = rand($max);
 #print "ranno = $ranno\n";
-$result = int($ranno) + 1;
+$result = int($ranno) + $offset + 1;
 print "$result\n";
 exit;
