e8bc9489d88b8ab9462bfe0f73461867309fd30d
marki <marki>
2014-07-25 13:26:28 +0000

    delete it

--- del_empty_dir.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-find -type d | perl -n -e 'chomp; $dir = $_; $dir =~ s/ /\\ /g; $dir =~ s/\(/\\\(/g; $dir =~ s/\)/\\\)/g; $count = `ls -a $dir | wc -l`; chomp $count; if ($count == 2) {system "rmdir -v $dir";}'
