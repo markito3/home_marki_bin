b2d63c7aa191d37bf00c127d54f8d0db07a3f644
marki <marki>
2015-05-25 23:09:18 +0000

    proper escapes, add verbose switch (at end)

--- diffdir.pl
+++ diffdir.pl
@@ -1,21 +1,40 @@
 #!/usr/bin/env perl
 $dir1 = $ARGV[0];
 $dir2 = $ARGV[1];
+#print "debug: dir1 = $dir1, dir2 = $dir2\n";
+$verbose = $ARGV[2];
 open(FIND1, "find $dir1 -type f |");
 while ($full = <FIND1>) {
+    #print "debug: full = $full";
     chomp $full;
     @token = split(/\//, $full);
+    #for ($i = 0; $i <= $#token; $i++) {print "token[$i] = $token[$i]\n";}
     $file = $token[$#token];
-    @token_base0 = split(/$dir1/, $full);
+    #print "debug: file = $file\n";
+    @token_base0 = split(/$dir1\//, $full);
+    #for ($i = 0; $i <= $#token_base0; $i++) {print "token_base0[$i] = $token_base0[$i]\n";}
     $base0 = $token_base0[1];
-    $file_for_split = $file;
-    $file_for_split =~ s/\+/\\\+/g;
-    $file_for_split =~ s/\./\\\./g;
-    @token_base = split(/$file_for_split/, $base0);
+    #print "debug: base0 = $base0\n";
+    $file_escaped = $file;
+    $file_escaped =~ s/\(/\\\(/g;
+    $file_escaped =~ s/\)/\\\)/g;
+    $file_escaped =~ s/\+/\\\+/g;
+    #print "debug: file_escaped = $file_escaped\n";
+    @token_base = split(/$file_escaped/, $base0);
+    #for ($i = 0; $i <= $#token_base; $i++) {print "token_base[$i] = $token_base[$i]\n";}
     $base = $token_base[0];
-    print "================$base$file=================\n";
-    $command = "diff -s $dir1/$base/$file $dir2/$base/$file";
-    print "$command\n";
+    #print "debug: base = $base\n";
+    if ($verbose) {print "================$base$file=================\n";}
+    if ($verbose) {$options = "-s"} else {$options = '';}
+    $file_escaped2 = $file_escaped;
+    $file_escaped2 =~ s/ /\\ /g;
+    $file_escaped2 =~ s/'/\\'/g;
+    $file_escaped2 =~ s/&/\\&/g;
+    $base_escaped = $base;
+    $base_escaped =~ s/ /\\ /g;
+    $base_escaped =~ s/'/\\'/g;
+    $command = "diff $options $dir1/${base_escaped}$file_escaped2 $dir2/${base_escaped}$file_escaped2";
+    if ($verbose) {print "$command\n";}
     system ("$command");
 }
 exit;
