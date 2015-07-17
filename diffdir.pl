0c3ad902d233f5ae056cd5b30080af7df7c1d854
marki <marki>
2015-07-17 13:07:36 +0000

    unescape periods, improve commented out debug printout lines

--- diffdir.pl
+++ diffdir.pl
@@ -8,20 +8,21 @@ while ($full = <FIND1>) {
     #print "debug: full = $full";
     chomp $full;
     @token = split(/\//, $full);
-    #for ($i = 0; $i <= $#token; $i++) {print "token[$i] = $token[$i]\n";}
+    #for ($i = 0; $i <= $#token; $i++) {print "debug: token[$i] = $token[$i]\n";}
     $file = $token[$#token];
     #print "debug: file = $file\n";
     @token_base0 = split(/$dir1\//, $full);
-    #for ($i = 0; $i <= $#token_base0; $i++) {print "token_base0[$i] = $token_base0[$i]\n";}
+    #for ($i = 0; $i <= $#token_base0; $i++) {print "debug: token_base0[$i] = $token_base0[$i]\n";}
     $base0 = $token_base0[1];
     #print "debug: base0 = $base0\n";
     $file_escaped = $file;
     $file_escaped =~ s/\(/\\\(/g;
     $file_escaped =~ s/\)/\\\)/g;
     $file_escaped =~ s/\+/\\\+/g;
+    $file_escaped =~ s/\./\\\./g;
     #print "debug: file_escaped = $file_escaped\n";
     @token_base = split(/$file_escaped/, $base0);
-    #for ($i = 0; $i <= $#token_base; $i++) {print "token_base[$i] = $token_base[$i]\n";}
+    #for ($i = 0; $i <= $#token_base; $i++) {print "debug: token_base[$i] = $token_base[$i]\n";}
     $base = $token_base[0];
     #print "debug: base = $base\n";
     if ($verbose) {print "================$base$file=================\n";}
@@ -30,9 +31,12 @@ while ($full = <FIND1>) {
     $file_escaped2 =~ s/ /\\ /g;
     $file_escaped2 =~ s/'/\\'/g;
     $file_escaped2 =~ s/&/\\&/g;
+    $file_escaped2 =~ s/\\\./\./g; # unescape periods
+    #print "debug: file_escaped2 = $file_escaped2\n";
     $base_escaped = $base;
     $base_escaped =~ s/ /\\ /g;
     $base_escaped =~ s/'/\\'/g;
+    #print "debug: base_escaped = $base_escaped\n";
     $command = "diff $options $dir1/${base_escaped}$file_escaped2 $dir2/${base_escaped}$file_escaped2";
     if ($verbose) {print "$command\n";}
     system ("$command");
