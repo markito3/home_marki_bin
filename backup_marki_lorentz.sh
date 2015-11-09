3c1240f2d6296e6cd1d151476844e1c6df82dd71
marki <marki>
2015-11-09 20:36:45 +0000

    improvements

--- backup_marki_lorentz.sh
+++ backup_marki_lorentz.sh
@@ -4,5 +4,9 @@ rm -rf backup.3
 mv -v backup.2 backup.3
 mv -v backup.1 backup.2
 cp -al backup.0 backup.1
-rsync -ruvt --delete /home/marki/ backup.0/ --exclude Music --exclude Renees_pictures.zip --exclude .cache --exclude .local/share/Trash --exclude .recoll/xapiandb --exclude .thunderbird --exclude Dropbox --exclude .local/share/gnome-boxes --exclude VirtualBox\ VMs --exclude google_drive --exclude iso
+rm -fv /home/marki/backup_timestamp.txt
+date > /home/marki/backup_timestamp.txt
+cat /home/marki/backup_timestamp.txt
+rsync -ruvt --delete /home/marki/ backup.0/ --exclude Music --exclude Renees_pictures.zip --exclude .cache --exclude .local/share/Trash --exclude .recoll/xapiandb --exclude .thunderbird --exclude Dropbox --exclude .local/share/gnome-boxes --exclude VirtualBox\ VMs --exclude google_drive --exclude iso --exclude .config/google-chrome | grep -v "skipping non-regular file"
+rm -v /home/marki/backup_timestamp.txt
 exit
