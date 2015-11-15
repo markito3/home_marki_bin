8bc694d72c4e91f960983ae5a764fa5590ea20eb
marki <marki>
2015-11-15 18:08:34 +0000

    changes

--- backup_marki.sh
+++ backup_marki.sh
@@ -4,5 +4,5 @@ rm -rf backup.3
 mv -v backup.2 backup.3
 mv -v backup.1 backup.2
 cp -al backup.0 backup.1
-rsync -ruvt --delete /home/marki/ backup.0/ --exclude Music --exclude Renees_pictures.zip --exclude .cache --exclude .local/share/Trash --exclude .recoll/xapiandb --exclude .thunderbird/7ao3ph9q.default/ImapMail/zmail.jlab.org/mail.sbd --exclude Dropbox
+rsync -ruvt --delete /home/marki/ backup.0/ --exclude Music --exclude Renees_pictures.zip --exclude .cache --exclude .local/share/Trash --exclude .recoll/xapiandb --exclude .thunderbird --exclude Dropbox
 exit
