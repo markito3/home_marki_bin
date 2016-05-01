9ce7c2584d67c1ceda0a463f9d421238d6cbc314
marki <marki>
2016-05-01 23:46:14 +0000

    changes

--- backup_marki.sh
+++ backup_marki.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-pushd /scratch/backup_marki
+pushd /home/backup/marki
 rm -rf backup.3
 mv -v backup.2 backup.3
 mv -v backup.1 backup.2
 cp -al backup.0 backup.1
-rsync -ruvt --delete /home/marki/ backup.0/ --exclude Music --exclude Renees_pictures.zip --exclude .cache --exclude .local/share/Trash --exclude .recoll/xapiandb --exclude .thunderbird --exclude Dropbox
+rsync -ruvt --delete /home/marki/ backup.0/ --exclude Music --exclude Renees_pictures.zip --exclude .cache --exclude .local/share/Trash --exclude .recoll/xapiandb --exclude .thunderbird --exclude Dropbox --exclude .config/google-chrome --exclude google_drive
 exit
--- setup_gluex.csh
+++ setup_gluex.csh
@@ -1,2 +1,3 @@
 setenv GLUEX_TOP /home/gluex/gluex_top
+setenv BUILD_SCRIPTS $GLUEX_TOP/build_scripts
 source $GLUEX_TOP/build_scripts/gluex_env_version.csh $GLUEX_TOP/version.xml
