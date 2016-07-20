64daf2f6e85d00804022a04ee25fc958d71c62d8
marki <marki>
2016-07-20 12:37:56 +0000

    go back to 5 minutes

--- coffee.sh
+++ coffee.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-seconds=$((1*5))
+seconds=$((60*5))
 sleep $seconds
 xmms /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga > /dev/null &
 display /home/marki/misc/ist2153798coffeecupoe6.jpg
