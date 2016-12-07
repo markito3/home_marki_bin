bbffb21f15174fbc458fe97f7015748741e13352
marki <marki>
2016-12-07 22:55:52 +0000

    update ip address

--- backup_rsync.sh
+++ backup_rsync.sh
@@ -4,5 +4,5 @@ rm -rf backup.3
 mv -v backup.2 backup.3
 mv -v backup.1 backup.2
 cp -al backup.0 backup.1
-rsync -ruvt --delete -e "ssh -i /home/marki/.ssh/renee_rsa" Renee@192.168.1.164:/home/Renee/ backup.0/ --exclude AppData --exclude Music/ogg
+rsync -ruvt --delete -e "ssh -i /home/marki/.ssh/renee_rsa" Renee@192.168.1.166:/home/Renee/ backup.0/ --exclude AppData --exclude Music/ogg
 exit
