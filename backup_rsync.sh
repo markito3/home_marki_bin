bc5394195afc46b9f79c8162115d213d69d7d934
marki <marki>
2017-12-27 02:11:20 +0000

    backup from Renee's Fedora desktop

--- backup_rsync.sh
+++ backup_rsync.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-ip=192.168.1.166
+ip=192.168.1.195
 pushd /run/media/marki/FantomHD/backup/renee
 rm -rf backup.3
 mv -v backup.2 backup.3
 mv -v backup.1 backup.2
 cp -al backup.0 backup.1
-ssh -i /home/marki/.ssh/renee_rsa Renee@$ip date \> backup_date
-rsync -ruvt --delete -e "ssh -i /home/marki/.ssh/renee_rsa" Renee@${ip}:/home/Renee/ backup.0/ --exclude AppData --exclude Music/ogg
+ssh -i /home/marki/.ssh/renee_rsa renee@$ip date \> backup_date
+rsync -ruvt --delete -e "ssh -i /home/marki/.ssh/renee_rsa" renee@${ip}:/home/renee/ backup.0/ --exclude .cache --exclude .config --exclude .local/share/Steam --exclude .mozilla
 exit
