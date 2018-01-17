aa7b17466a69845f9b312e2f75e0d0a75a9291e9
marki <marki>
2018-01-17 00:01:20 +0000

    exclude .steam

--- backup_rsync.sh
+++ backup_rsync.sh
@@ -6,5 +6,5 @@ mv -v backup.2 backup.3
 mv -v backup.1 backup.2
 cp -al backup.0 backup.1
 ssh -i /home/marki/.ssh/renee_rsa renee@$ip date \> backup_date
-rsync -ruvt --delete -e "ssh -i /home/marki/.ssh/renee_rsa" renee@${ip}:/home/renee/ backup.0/ --exclude .cache --exclude .config --exclude .local/share/Steam --exclude .mozilla
+rsync -ruvt --delete -e "ssh -i /home/marki/.ssh/renee_rsa" renee@${ip}:/home/renee/ backup.0/ --exclude .cache --exclude .config --exclude .local/share/Steam --exclude .mozilla --exclude .steam
 exit
