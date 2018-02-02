087e6d6f7f27e3463b086b0338433005ce6e2d08
marki <marki>
2018-02-02 17:06:17 +0000

    exclude music directories

--- gluex_install_scratch.sh
+++ gluex_install_scratch.sh
@@ -3,7 +3,10 @@ pushd $1
 mkdir -pv gluex_install_scratch
 pushd gluex_install_scratch
 day=`date +%u`
+logfile=gluex_install_${day}.log
 rm -rf $day
 mkdir -pv $day
 pushd $day
-/home/marki/gluex_install/gluex_install.sh
+echo installing in `pwd`
+echo log file is $logfile
+/home/marki/gluex_install/gluex_install.sh >& $logfile
