cf559a8e88eb80d2eced9116e9fb7d7347b17840
marki <marki>
2018-08-13 00:49:10 +0000

    do 6 backups

--- backup_rsync.sh
+++ backup_rsync.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 ip=192.168.1.195
 pushd /run/media/marki/Travel/backup/renee
-rm -rf backup.3
+rm -rf backup.6
+mv -v backup.5 backup.6
+mv -v backup.4 backup.5
+mv -v backup.3 backup.4
 mv -v backup.2 backup.3
 mv -v backup.1 backup.2
 cp -al backup.0 backup.1
@@ -9,5 +12,5 @@ ssh -i /home/marki/.ssh/renee_rsa renee@$ip date \> backup_date
 rsync -ruvt --delete -e "ssh -i /home/marki/.ssh/renee_rsa" \
       renee@${ip}:/home/renee/ backup.0/ --exclude .cache --exclude .config \
       --exclude .local/share/Steam --exclude .mozilla --exclude .steam \
-      --exclude .local/share/gvfs-metadata --exclude .local/share/tracker
+      --exclude .local/share/gvfs-metadata --exclude .local/share/tracker --exclude Music --exclude Dropbox/.dropbox.cache
 exit
