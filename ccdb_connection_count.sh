9797758215df81b0c6899f6052be8817d4abd295
marki <marki>
2018-10-11 17:08:44 +0000

    changes

--- ccdb_connection_count.sh
+++ ccdb_connection_count.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mysql -hhallddb -umarki -phybrid information_schema -e 'select user, count(*) from processlist group by user'
-mysql -hhallddb -uccdb_user information_schema -e 'select count(*) from processlist where user = "ccdb_user" and host like "farm%"'
+mysql -hhallddb -uccdb_user information_schema -e 'select count(*) from processlist where user = "ccdb_user" and (host like "farm%" or host like "qcd%")'
