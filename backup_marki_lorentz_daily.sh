1f077412e780f184c49cd78b6046307b41ac5332
marki <marki>
2020-06-17 18:44:34 +0000

    add two more days

--- backup_marki_lorentz_daily.sh
+++ backup_marki_lorentz_daily.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 pushd /local/backup/home/marki
-rm -rf daily.2
+rm -rf daily.4
+mv -v daily.3 daily.4
+mv -v daily.2 daily.3
 mv -v daily.1 daily.2
 mv -v daily.0 daily.1
 cp -al backup.0 daily.0
