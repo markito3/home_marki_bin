11c3dbc1d2ae4eec84a44a2ca4bb8cd52bc894ca
marki <marki>
2020-06-18 13:30:13 +0000

    add option for displaying LD_LIBRARY_PATH

--- showpath.pl
+++ showpath.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/env perl
-$line=$ENV{'PATH'};
+if ($ARGV[0]) {
+    $line=$ENV{'LD_LIBRARY_PATH'};
+} else {
+    $line=$ENV{'PATH'};
+}
 #print "$line\n";
 @field = split(/:/,$line);
 print "number of fields $#field\n";
