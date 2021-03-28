130de14371865fdb67c802ba42f789e6b4d8b378
marki <marki>
2021-03-28 01:16:35 +0000

    backup silently

--- backup_marki.sh
+++ backup_marki.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 rm -f /home/marki/date.txt
 date > /home/marki/date.txt
-pushd /data/marki/backups/markdesk5/home/marki
+cd /data/marki/backups/markdesk5/home/marki
 rm -rf backup.11
-mv -v backup.10 backup.11
-mv -v backup.9 backup.10
-mv -v backup.8 backup.9
-mv -v backup.7 backup.8
-mv -v backup.6 backup.7
-mv -v backup.5 backup.6
-mv -v backup.4 backup.5
-mv -v backup.3 backup.4
-mv -v backup.2 backup.3
-mv -v backup.1 backup.2
+mv backup.10 backup.11
+mv backup.9 backup.10
+mv backup.8 backup.9
+mv backup.7 backup.8
+mv backup.6 backup.7
+mv backup.5 backup.6
+mv backup.4 backup.5
+mv backup.3 backup.4
+mv backup.2 backup.3
+mv backup.1 backup.2
 cp -al backup.0 backup.1
-rsync -ruvtl --delete --exclude-from=/home/marki/bin/backup_exclude.txt /home/marki/ /data/marki/backups/markdesk5/home/marki/backup.0/
+rsync -rutl --delete --exclude-from=/home/marki/bin/backup_exclude.txt /home/marki/ /data/marki/backups/markdesk5/home/marki/backup.0/
