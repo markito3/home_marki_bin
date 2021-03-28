848dce793e2c41ac0504070828b1eee6b25991b3
marki <marki>
2021-03-28 01:16:49 +0000

    backup for a year

--- backup_marki_monthly.sh
+++ backup_marki_monthly.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 pushd /data/marki/backups/markdesk5/home/marki
-rm -rf monthly.5
+rm -rf monthly.11
+mv -v monthly.10 monthly.11
+mv -v monthly.9 monthly.10
+mv -v monthly.8 monthly.9
+mv -v monthly.7 monthly.8
+mv -v monthly.6 monthly.7
+mv -v monthly.5 monthly.6
 mv -v monthly.4 monthly.5
 mv -v monthly.3 monthly.4
 mv -v monthly.2 monthly.3
