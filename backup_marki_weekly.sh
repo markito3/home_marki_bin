b9771f019c3773bd64898625ecda98f9e3808584
Mark Ito <markito3@gmail.com>
2022-05-07 15:01:29 -0400

    changes

--- backup_marki_weekly.sh
+++ backup_marki_weekly.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 pushd /data/marki/backups/markdesk5/home/marki
 rm -rf weekly.3
-mv -v weekly.2 weekly.3
-mv -v weekly.1 weekly.2
-mv -v weekly.0 weekly.1
+mv weekly.2 weekly.3
+mv weekly.1 weekly.2
+mv weekly.0 weekly.1
 cp -al backup.0 weekly.0
 exit
--- self_timer.pl
+++ self_timer.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/env perl
-$i = 0;
+$i = 1;
 $total_minutes = 0;
 while (1) {
-    $minutes = 5*2**$i;
+    $minutes = 2**$i;
     system "/home/marki/bin/alarm.sh $minutes";
     $total_minutes += $minutes;
-    $next_minutes = 5*2**($i + 1);
+    $next_minutes = 2**($i + 1);
     print "\n$total_minutes so far, $next_minutes more? <enter> = yes, n = no: ";
     $response = <STDIN>;
     if ($response =~ /n/) {exit;}
